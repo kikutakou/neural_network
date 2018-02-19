916709081aff7cb891a95315f4a4ac1688827685
kikutakou <kikutakou@gmail.com>
2018-02-19 18:30:28 +0800

    refactor gradient

--- grandients_for_w.py
+++ grandients_for_w.py
@@ -9,34 +9,30 @@ from modules.load_data import Data
 import time
 
 
+# argparse
 parser = argparse.ArgumentParser()
 parser.add_argument("data", help="data per line, [x[0],x[1],label] data separated by tab")
 args = parser.parse_args()
 data = Data(args.data)
 
 
-
 # plot
+plt.main_ax(xrange=[-5, 5], yrange=[-5, 5])
 plt.plot3d(*data.x1)
 
 
-
 # mesh for gradients of w
-mesh_x = np.arange(-5, 5.1, 0.1)
-mesh_y = np.arange(-5, 5.1, 0.1)
-mesh = np.meshgrid(mesh_x, mesh_y)
+mesh = np.meshgrid(np.arange(-5, 5.1, 0.1), np.arange(-5, 5.1, 0.1))
 
 # dot products for N x MESH
 u = sum(np.expand_dims(ww, axis=2) * xx for ww, xx in zip(mesh, data.x1))
 
 # gradients
-Ew = ((u - 1) ** 2).mean(axis=2)           #linear
-#Ew = ((sigmoid(u) - 1) ** 2).mean(axis=2)       #sigmoid
+#Ew = ((u - 1) ** 2).mean(axis=2)           #linear
+Ew = ((sigmoid(u) - 1) ** 2).mean(axis=2)       #sigmoid
+
 
 
 plt.mesh3d(*mesh, Ew)
-
-
-
 plt.show()
 
